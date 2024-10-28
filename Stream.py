f5019554ccf5d3bc0a86a20595d60fa1a8e55262
danvaz313 <danisantos313sv@gmail.com>
2024-10-28 10:35:25 -0600

    Version Inicial

---
+++ Stream.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="Monitoreo Industrial",
+    layout="wide"
+)
+
+st.title("Sistema de Monitoreo Industrial")
+st.write("Bienvenido al sistema de Monitoreo en Tiempo Real")
